ed537eac3090f08b5b7cc8f35ac737eea6cfca3b
Hongjian Li <JackyLeeHongJian@Gmail.com>
2018-11-08 11:12:28 +0800

    Added convert.sh

---
+++ results/plots/convert.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+for s in "structural similarity" "sequence similarity"
+do
+	echo "$s"
+	cd "$s"
+	for d in "training with most dissimilar samples" "training with most similar samples"
+	do
+		echo "$d"
+		cd "$d"
+		for f in "X-Score" "Vina" "Cyscore"
+		do
+			echo "$f"
+			cd "$f"
+			for m in "RP" "RS" "RMSE"
+			do
+				echo "$m"
+				convert -density 600 -trim $m.pdf $m.png
+				#convert -density 600 -trim -compress lzw $m.pdf $m.tiff
+			done
+			cd ..
+		done
+		cd ..
+	done
+	cd ..
+done
